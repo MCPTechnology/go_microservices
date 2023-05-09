be40e81b0eeb093a9c88b1764c64135efee3b6e8
Matheus Caputo Pires <matheus.piresufv@gmail.com>
2023-05-09 08:35:19 -0300

    Update main.go

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -7,15 +7,22 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"syscall"
 	"time"
 
-	appHandlers "github.com/MCPTechnology/go_microservices/internal/app/handlers"
+	inventoryHandler "github.com/MCPTechnology/go_microservices/internal/app/handlers/inventory"
+	"github.com/MCPTechnology/go_microservices/scripts/seeds"
 	"github.com/gorilla/mux"
 )
 
 const (
-	ip   string = ""
-	port int32  = 9090
+	ip                     string        = ""
+	port                   int32         = 9090
+	appName                string        = "products_api_"
+	idleTimeout            time.Duration = 120 * time.Second
+	readTimeout            time.Duration = 1 * time.Second
+	writeTimeout           time.Duration = 1 * time.Second
+	gracefulShutdownPeriod time.Duration = 30 * time.Second
 )
 
 var addr string = fmt.Sprintf("%v:%v", ip, port)
@@ -26,8 +33,8 @@ func main() {
 	postRouter := serveMux.Methods(http.MethodPost).Subrouter()
 	putRouter := serveMux.Methods(http.MethodPut).Subrouter()
 
-	logger := log.New(os.Stdout, "product-api", log.LstdFlags)
-	products := appHandlers.NewProducts(logger)
+	logger := log.New(os.Stdout, appName, log.LstdFlags)
+	products := inventoryHandler.NewInventoryHandler(logger, seeds.SeedProducts()...)
 	postRouter.Use(products.MiddlewareProductValidation)
 	putRouter.Use(products.MiddlewareProductValidation)
 
@@ -42,8 +49,7 @@ func main() {
 		ReadTimeout:  1 * time.Second,
 		WriteTimeout: 1 * time.Second,
 	}
-
-	//* Start the Server
+	// * Starting server
 	go func() {
 		logger.Println("Starting server on port ", port)
 		err := server.ListenAndServe()
@@ -52,17 +58,17 @@ func main() {
 		}
 	}()
 
-	// Trap sigterm or interrupt and gracefully shutdown the server
+	// * Trap termination or interrupt signals and gracefully shutdown the server
 	signalChannel := make(chan os.Signal, 1)
 	signal.Notify(signalChannel, os.Interrupt)
-	signal.Notify(signalChannel, os.Kill)
+	signal.Notify(signalChannel, syscall.SIGTERM)
 
-	// Block until a signal is received
+	// * Block until a signal is received
 	signal := <-signalChannel
-	logger.Printf("Received terminate signal: %v\nGraceful shutdon...\n", signal)
+	logger.Printf("Received terminate signal: %v\n  Graceful shutdown...\n", signal)
 
-	// Gracefully shutdown the server, waiting a max of 30 seconds for running operations to close
-	timeoutContext, cancelFunc := context.WithTimeout(context.Background(), 30*time.Second)
+	// * Gracefully shutdown the server, waiting for running operations to close
+	timeoutContext, cancelFunc := context.WithTimeout(context.Background(), gracefulShutdownPeriod)
 	defer cancelFunc()
 	server.Shutdown(timeoutContext)
 }
